59285e6e0f285be07a7c9ce75690b318cfc77bf4
alexeyvokin <alvo14@mail.ru>
2021-08-05 15:50:20 +0700

    feat: use form model for data

--- src/components/ProductsAddForm.vue
+++ src/components/ProductsAddForm.vue
@@ -1,34 +1,59 @@
 <script>
 import BaseInput from './BaseInput.vue';
 import BaseTextarea from './BaseTextarea.vue';
+import BaseButton from './BaseButton.vue';
 import FormItem from './FormItem.vue';
+
+const createFormModel = () => ({
+  name: '',
+  description: '',
+  preview: '',
+  price: '',
+});
 
 export default {
   name: 'ProductsAddForm',
+
   components: {
     BaseInput,
     BaseTextarea,
+    BaseButton,
     FormItem,
+  },
+
+  data() {
+    return {
+      form: createFormModel(),
+    };
+  },
+
+  methods: {
+    handleSubmit() {
+      this.$emit('submit', { ...this.form });
+      this.form = createFormModel();
+    },
   },
 };
 </script>
 
 <template>
-  <form class="products-add-form">
+  <form class="products-add-form" @submit.prevent="handleSubmit">
     <div class="products-add-form__fields">
       <FormItem label="Наименование товара" required>
-        <BaseInput type="text" placeholder="Введите наименование товара" />
+        <BaseInput v-model="form.name" type="text" placeholder="Введите наименование товара" />
       </FormItem>
       <FormItem label="Описание товара">
-        <BaseTextarea placeholder="Введите описание товара" />
+        <BaseTextarea v-model="form.description" placeholder="Введите описание товара" />
       </FormItem>
       <FormItem label="Ссылка на изображение товара" required>
-        <BaseInput type="text" placeholder="Введите ссылку" />
+        <BaseInput v-model="form.preview" type="text" placeholder="Введите ссылку" />
       </FormItem>
       <FormItem label="Цена товара" required>
-        <BaseInput type="text" placeholder="Введите цену" />
+        <BaseInput v-model="form.price" type="text" placeholder="Введите цену" />
       </FormItem>
     </div>
+
+    <BaseButton type="submit">Добавить товар</BaseButton>
   </form>
 </template>
 
