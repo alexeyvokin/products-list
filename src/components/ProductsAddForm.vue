62dd88873cb4883d120307e479f86a37456ef9e4
alexeyvokin <alvo14@mail.ru>
2021-08-06 15:54:23 +0700

    feat: use vee-validate for form validation

--- src/components/ProductsAddForm.vue
+++ src/components/ProductsAddForm.vue
@@ -1,4 +1,6 @@
 <script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
+
 import BaseInput from './BaseInput.vue';
 import BaseTextarea from './BaseTextarea.vue';
 import BaseButton from './BaseButton.vue';
@@ -15,6 +17,8 @@ export default {
   name: 'ProductsAddForm',
 
   components: {
+    ValidationObserver,
+    ValidationProvider,
     BaseInput,
     BaseTextarea,
     BaseButton,
@@ -37,24 +41,39 @@ export default {
 </script>
 
 <template>
-  <form class="products-add-form" @submit.prevent="handleSubmit">
+  <ValidationObserver
+    v-slot="{ invalid }"
+    tag="form"
+    class="products-add-form"
+    @submit.prevent="!invalid && handleSubmit()"
+  >
+    {{ invalid }}
     <div class="products-add-form__fields">
       <FormItem label="Наименование товара" required>
-        <BaseInput v-model="form.name" name="name" type="text" placeholder="Введите наименование товара" />
+        <ValidationProvider v-slot="{ errors }" rules="required">
+          <BaseInput v-model="form.name" name="name" type="text" placeholder="Введите наименование товара" />
+          {{ errors }}
+        </ValidationProvider>
       </FormItem>
       <FormItem label="Описание товара">
         <BaseTextarea v-model="form.description" name="description" placeholder="Введите описание товара" />
       </FormItem>
       <FormItem label="Ссылка на изображение товара" required>
-        <BaseInput v-model="form.preview" name="preview" type="text" placeholder="Введите ссылку" />
+        <ValidationProvider v-slot="{ errors }" rules="required|url">
+          <BaseInput v-model="form.preview" name="preview" type="text" placeholder="Введите ссылку" />
+          {{ errors }}
+        </ValidationProvider>
       </FormItem>
       <FormItem label="Цена товара" required>
-        <BaseInput v-model.number="form.price" name="price" type="text" placeholder="Введите цену" />
+        <ValidationProvider v-slot="{ errors }" rules="required">
+          <BaseInput v-model.number="form.price" name="price" type="text" placeholder="Введите цену" />
+          {{ errors }}
+        </ValidationProvider>
       </FormItem>
     </div>
 
-    <BaseButton type="submit">Добавить товар</BaseButton>
-  </form>
+    <BaseButton type="submit" :disabled="invalid">Добавить товар</BaseButton>
+  </ValidationObserver>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/plugins/vee-validate.js
@@ -0,0 +1,27 @@
+import { extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+
+function isValidHttpUrl(string) {
+  try {
+    const url = new URL(string);
+    return ['http:', 'https:'].includes(url.protocol);
+  } catch (_) {
+    return false;
+  }
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export function registerVeeValidateRules() {
+  extend('required', {
+    ...required,
+    message: 'Поле обязательно для заполнения',
+  });
+
+  extend('url', value => {
+    if (isValidHttpUrl(value)) {
+      return true;
+    }
+
+    return 'Поле должно содержать url-адрес, начинающийся с http';
+  });
+};
